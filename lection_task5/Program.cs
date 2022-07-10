930ef66cbc9074cf2f2cb155b8bab7b786688f18
Aleksey <kozlovav78@mail.ru>
2022-07-10 23:51:43 +0300

    Решение задачи 5 из семинара

---
+++ lection_task5/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число: ");
+string str = Console.ReadLine();
+int n = str.Length;
+if (n == 3) 
+{    
+    Console.WriteLine("Последняя цифра числа равна ");  
+    Console.WriteLine(str[n-1]); 
+}
+else 
+    Console.WriteLine("Вы ввели не трехзначное число ");
